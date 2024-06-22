86bd695808358439525359adbd2c75c439afdacf
ashikvpapanna@gmail.com <codekeyword@gmail.com>
2024-06-22 12:01:25 +0530

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class= "test" style="height: 710px;width:720px;background-color:transparent">
+
+</div>
+<style>
+   .test{
+    background-image: linear-gradient(45deg, #808080 25%, transparent 25%), linear-gradient(-45deg, #808080 25%, transparent 25%), linear-gradient(45deg, transparent 75%, #808080 75%), linear-gradient(-45deg, transparent 75%, #808080 75%);
+  background-size: 20px 20px;
+  background-position: 0 0, 0 10px, 10px -10px, -10px 0px;
+   }  
+</style>
